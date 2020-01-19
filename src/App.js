6b1c53d2a95343aa5a4c5a8184f53c1b6fdf93fc
German Manalili <german@invento.io>
2020-01-19 22:33:24 +0800

    Added mobile detection

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Container from './components/Container'
 import Navbar from './screens/Navbar'
 import Footer from './screens/Footer'
 import Content from './screens/Content'
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect"
 
 const App = () => {
   return (
     <Container>
-      <Navbar />
-      <Content />
-      <Footer />
+      <BrowserView>
+        <Navbar />
+        <Content />
+        <Footer />
+      </BrowserView>
+      <MobileView>
+        <h1>I have not yet implemented mobile support for this site please use a computer or change to desktop mode.</h1>
+      </MobileView>
     </Container>
   );
 }
